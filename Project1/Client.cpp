657f6746c1dba0b5015c89417c1e30d830aad6e7
seif <92771610+Seif357@users.noreply.github.com>
2023-12-15 20:44:38 +0200

    Update Client.cpp

--- Project1/Client.cpp
+++ Project1/Client.cpp
@@ -3,29 +3,51 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
+#include "Decryption.h"
+#include "Encryption.h"
+#include "utils.h"
 
 #define PORT 9909
+#define BUFFER_SIZE 256
+#define SERVER_IP "127.0.0.1"
+#define EXIT_KEYWORD "\\exit"
 using namespace std;
 int nClientSocket;
 struct sockaddr_in srv;
-char buff[255] = { 0, };
+char buff[BUFFER_SIZE] = { 0, };
+bool recvcontinuation = true; // Set the flag to continue loop1
 
-std::mutex mtx;
+mutex mtx;
 
-void loop1() {
-    while (true) {
+void recive() {
+    while (recvcontinuation) {
         this_thread::sleep_for(chrono::seconds(1));
-        recv(nClientSocket, buff, 256, 0);
+        int bytesReceived = recv(nClientSocket, Decrypt(buff, 20,15), BUFFER_SIZE - 1, 0);
+        if (bytesReceived == SOCKET_ERROR || bytesReceived == 0)
+        {
+            cout << "Error receiving response from the server" << endl;
+            WSACleanup();
+            exit(EXIT_SUCCESS);
+        }
         std::lock_guard<std::mutex> lock(mtx);
-        cout << endl << buff << endl;
+        cout << buff << endl;
     }
 }
 
-void loop2() {
+void sending() {
     while (true) {
         this_thread::sleep_for(chrono::seconds(1));
         fgets(buff, 256, stdin);
-        send(nClientSocket, buff, 256, 0);
+        if (strncmp(buff, EXIT_KEYWORD, strlen(EXIT_KEYWORD)) == 0)
+        {
+            recvcontinuation = false;
+            WSACleanup();
+            exit(EXIT_SUCCESS);
+        }
+        size_t len = strlen(buff);
+        if (len > 0 && buff[len - 1] == '\n')
+            buff[len - 1] = '\0';
+        send(nClientSocket, Encrypt(buff, 15, 20), len, 0);
     }
 }
 
@@ -35,7 +57,7 @@ int main()
     WSAData ws;
     if (WSAStartup(MAKEWORD(2, 2), &ws) < 0)
     {
-        cout << endl << "WSAStartup failed";
+        cout << "WSAStartup failed" << endl;
         WSACleanup();
         return (EXIT_FAILURE);
     }
@@ -44,38 +66,38 @@ int main()
 
     if (nClientSocket < 0)
     {
-        cout << endl << "socket call failed";
+        cout <<"socket call failed" << endl;
         WSACleanup();
         return (EXIT_FAILURE);
     }
     srv.sin_family = AF_INET;
     srv.sin_port = htons(PORT);
-    srv.sin_addr.s_addr = inet_addr("127.0.0.1");
+    srv.sin_addr.s_addr = inet_addr(SERVER_IP);
     memset(&srv.sin_zero, 0, 8);
     nRet = connect(nClientSocket, (struct sockaddr*)&srv, sizeof(srv));
     if (nRet < 0)
     {
-        cout << endl << "connect failed";
+        cout <<"connect failed" << endl;
         WSACleanup();
         return (EXIT_FAILURE);
     }
     else
     {
-        cout << endl << "Connected to the server ";
+       //connected to the sever
         char buff[255] = { 0, };
         char userMessage[255] = { 0, };
         // Concatenate the predefined message and user input
         recv(nClientSocket, buff, 256, 0);
-        std::cout << endl << "Just press enter to see the message received from the server";
+        cout <<"Just press enter to see the message received from other clients" << endl;
         cin.ignore(); // Ignore any newline character left in the buffer
-        cout << endl << buff;
-        cout << endl << "Now send your message to server:";
+        cout<< "type " << EXIT_KEYWORD << " to quit";
+        cout << buff << endl;
+        //now send what you want
         // Create two threads
-        thread thread1(loop1);
-        thread thread2(loop2);
+        thread thread2(sending);
+        thread thread1(recive);
         // Wait for both threads to finish (which will never happen in this case)
-        thread1.join();
         thread2.join();
-
+        thread1.join();
     }
 }
