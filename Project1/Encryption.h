7e27775a4f7a67e6c96dc8f3762d1f2dea1651ea
seif <92771610+Seif357@users.noreply.github.com>
2023-12-18 05:10:05 +0200

    Update Encryption.h

--- Project1/Encryption.h
+++ Project1/Encryption.h
@@ -1,32 +1,23 @@
 #pragma once
-#include "Utils.h"
+#include "utils.h"
+
+
 std::string Encrypt(std::string str, int right_Shift, int left_shift) {
     std::string encrypted;
-    std::vector<int> positions;
     encrypted = string_to_lower(str);
+
     for (int i = 0; i < encrypted.length(); i += 2) {
         if (isalpha(encrypted[i])) {
-            encrypted[i] = encrypted[i] + right_Shift;
-            if (encrypted[i] > 122) {
-                encrypted[i] = encrypted[i] - 26;
-            }
-            else if (encrypted[i] < 97) {
-                encrypted[i] = encrypted[i] + 26;
-            }
+            encrypted[i] = (encrypted[i] - ASCII_LOWER_A + right_Shift) % ALPHABET_SIZE + ASCII_LOWER_A;
+        }
+    }
 
-        }
-    }
     for (int i = 1; i < encrypted.length(); i += 2) {
-        if (encrypted[i] > 91 && encrypted[i] < 126) {
-            encrypted[i] = encrypted[i] - left_shift;
-            if (encrypted[i] > 126) {
-                encrypted[i] = encrypted[i] - 36;
-            }
-            else if (encrypted[i] < 91) {
-                encrypted[i] = encrypted[i] + 36;
-            }
+        if (isalpha(encrypted[i])) {
+            encrypted[i] = (encrypted[i] - ASCII_LOWER_A - left_shift + ALPHABET_SIZE) % ALPHABET_SIZE + ASCII_LOWER_A;
         }
     }
+
     std::reverse(encrypted.begin(), encrypted.end());
     return encrypted;
 }
