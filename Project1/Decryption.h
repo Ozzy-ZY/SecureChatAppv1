2eedc6792ba47f934e031691bedfde95d6e5b129
seif <92771610+Seif357@users.noreply.github.com>
2023-12-18 05:09:44 +0200

    Update Decryption.h

--- Project1/Decryption.h
+++ Project1/Decryption.h
@@ -1,37 +1,26 @@
 #pragma once
 #include "utils.h"
-void Decrypt(char* str, int shift_right, int shift_left) {
-    if (str == nullptr) {
-        std::cout << "Error: null pointer" << std::endl;
-        return;
-    }
-    std::reverse(str, str + strlen(str));
 
-    for (int i = 0; i < strlen(str); i += 2) {
-        if (isalpha(str[i])) {
-            str[i] = str[i] - shift_right;
-            if (str[i] > 122) {
-                str[i] = str[i] - 26;
-            }
-            else if (str[i] < 97) {
-                str[i] = str[i] + 26;
-            }
+std::string Decrypt(std::string str, int right_Shift, int left_shift) {
+    std::string decrypted;
+    decrypted = str;  // Start with the encrypted string
+
+    // Reverse the string to its original order
+    std::reverse(decrypted.begin(), decrypted.end());
+
+    for (int i = 1; i < decrypted.length(); i += 2) {
+        if (isalpha(decrypted[i])) {
+            // Reverse the left shift operation
+            decrypted[i] = (decrypted[i] - ASCII_LOWER_A + left_shift) % ALPHABET_SIZE + ASCII_LOWER_A;
         }
-        else
-            continue;
     }
 
-    for (int i = 1; i < strlen(str); i += 2) {
-        if (str[i] > 91 && str[i] < 126) {
-            str[i] = str[i] + shift_left;
-            if (str[i] > 126) {
-                str[i] = str[i] - 36;
-            }
-            else if (str[i] < 91) {
-                str[i] = str[i] + 36;
-            }
+    for (int i = 0; i < decrypted.length(); i += 2) {
+        if (isalpha(decrypted[i])) {
+            // Reverse the right shift operation
+            decrypted[i] = (decrypted[i] - ASCII_LOWER_A - right_Shift + ALPHABET_SIZE) % ALPHABET_SIZE + ASCII_LOWER_A;
         }
-        else
-            continue;
     }
+
+    return decrypted;
 }
